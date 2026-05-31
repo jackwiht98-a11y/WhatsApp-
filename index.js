801b011e82cf4595ae9a4c34f953da26a00708e1
jackwiht98-a11y <jack.wiht.98@gmail.com>
2026-05-31 17:01:06 -0600

    index.js

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
 const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: 'session'
-    }),
+    authStrategy: new LocalAuth({ dataPath: 'session' }),
     puppeteer: {
         headless: true,
+        executablePath: '/usr/bin/chromium',
         args: [
             '--no-sandbox',
-            '--disable-setuid-sandbox',
+            '--disable-setuid-sandbox', 
             '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
+            '--disable-gpu',
             '--no-zygote',
-            '--single-process',
-            '--disable-gpu'
-        ],
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+            '--single-process'
+        ]
     }
 });
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
